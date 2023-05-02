097bb736c3245a400a719f0f9a8b581592c356c2
hamid313-coder <hamid.hussainy0@gmail.com>
2023-05-02 15:00:48 +0430

    did the magic ball chanllenge

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,40 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(
     const MaterialApp(
-      home: null,
+      home: MagicBall(),
     ),
   );
+}
+
+class MagicBall extends StatefulWidget {
+  const MagicBall({super.key});
+
+  @override
+  State<MagicBall> createState() => _MagicBallState();
+}
+
+class _MagicBallState extends State<MagicBall> {
+  static int getRandom() => Random().nextInt(5) + 1;
+  int number = getRandom();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue.shade400,
+      appBar: AppBar(
+        title: const Text("Magic Ball"),
+        backgroundColor: Colors.blue,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Center(
+            child: GestureDetector(
+                onTap: () => setState(() => number = getRandom()),
+                child: Image.asset("images/ball$number.png"))),
+      ),
+    );
+  }
 }
